fc75f95f5e5eeea7a1f3845b01cceb7119e2378f
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-03-11 19:12:42 -0500

    added button effects

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const button = document.querySelectorAll('.button');
+const operator = document.querySelectorAll('.operator');
 
+operator.forEach((operator) => {
+    operator.addEventListener('mousedown', () => {
+        operator.style.backgroundColor = 'rgb(255, 206, 92)';
+    });
+    operator.addEventListener('mouseup', () => {
+        operator.style.backgroundColor = 'rgb(237, 154, 0)';
+    });
+});
+
+button.forEach((button) => {
+    button.addEventListener('mousedown', () => {
+        button.style.backgroundColor = 'rgb(147, 147, 147)';
+    });
+    button.addEventListener('mouseup', () => {
+        button.style.backgroundColor = 'black';
+    });
+});
