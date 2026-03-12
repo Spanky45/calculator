cedaa93f1e04a179fe2a68a6deea3545e46f070d
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-03-12 16:03:43 -0500

    not much changed. switching pcs.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const button = document.querySelectorAll('.button');
 const operator = document.querySelectorAll('.operator');
 const calScreen = document.querySelector('.cal-screen');
 const calText = document.querySelector('.cal-text');
-
+const inputs = [];
 
 function updateCalText() {
 
@@ -12,6 +12,8 @@ function updateCalText() {
 operator.forEach((operator) => {
     operator.addEventListener('mousedown', () => {
         operator.style.backgroundColor = 'rgb(255, 206, 92)';
+        const value = operator.textContent;
+        console.log(value);
     });
     operator.addEventListener('mouseup', () => {
         operator.style.backgroundColor = 'rgb(237, 154, 0)';
